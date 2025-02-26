c8648cc0b0691beaf52b3d35cc08864c1424d25d
permalik <permalik@protonmail.com>
2025-02-25 22:19:14 -0600

    feat: update error handling

--- bulk_rename/src/index.js
+++ bulk_rename/src/index.js
@@ -1,12 +1,28 @@
-import { readdir } from "node:fs/promises";
+import { copyFile, constants, readdir, stat } from "node:fs/promises";
 
 const SOURCEDIRECTORY = "data";
 
 try {
-  const files = await readdir(SOURCEDIRECTORY);
-  for (const file of files) {
-    console.log(file);
+  let files = [];
+  const sourceDir = await stat(SOURCEDIRECTORY).catch((err) => {
+    throw new Error(`Failed to access sourceDir.\n${err.message}`);
+  });
+
+  if (sourceDir && sourceDir.isDirectory()) {
+    files = await readdir(SOURCEDIRECTORY).catch((err) => {
+      throw new Error(`Failed to read directory.\n${err.message}`);
+    });
+  } else {
+    console.log(`Directory does not exist.\n${SOURCEDIRECTORY}`);
+  }
+
+  if (files.length > 0) {
+    for (const file of files) {
+      console.log(file);
+    }
+  } else {
+    console.log(`No files found in directory.\n${SOURCEDIRECTORY}`);
   }
 } catch (err) {
-  console.error("Failed to read file from directory.\n", err);
+  console.error(`Error.\n${err.message}`);
 }
