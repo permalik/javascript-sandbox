d52a02946da7a082e9860c4e312e6797a9a069c7
permalik <permalik@protonmail.com>
2025-02-26 06:26:51 -0600

    feat: bulk_rename error handling, createDir

--- bulk_rename/src/index.js
+++ bulk_rename/src/index.js
@@ -1,19 +1,24 @@
 import { copyFile, constants, readdir, stat, mkdir } from "node:fs/promises";
 
 const SOURCEDIRECTORY = "data";
-const DESTDIRECTORY = "";
+const DESTDIRECTORY = "dest";
 
 try {
   let files = [];
 
-  createDir().catch((err) => console.err(err));
+  const sourceDir = await createDir(SOURCEDIRECTORY).catch((err) =>
+    console.error(err),
+  );
+  const destDir = await createDir(DESTDIRECTORY).catch((err) =>
+    console.error(err),
+  );
 
   if (sourceDir && sourceDir.isDirectory()) {
     files = await readdir(SOURCEDIRECTORY).catch((err) => {
-      throw new Error(`Failed to read directory.\n${err.message}`);
+      throw new Error(`Failed to read directory.\n${err.message}\n`);
     });
   } else {
-    console.log(`Directory does not exist.\n${SOURCEDIRECTORY}`);
+    console.log(`Directory does not exist.\n${SOURCEDIRECTORY}\n`);
   }
 
   if (files.length > 0) {
@@ -21,22 +26,21 @@ try {
       console.log(file);
     }
   } else {
-    console.log(`No files found in directory.\n${SOURCEDIRECTORY}`);
+    console.log(`No files found in directory.\n${SOURCEDIRECTORY}\n`);
   }
 } catch (err) {
-  console.error(`Error.\n${err.message}`);
+  console.error(`Error.\n${err.message}\n`);
 }
 
-async function createDir() {
-  let sourceDir;
+async function createDir(dir) {
   try {
-    sourceDir = await stat(SOURCEDIRECTORY);
+    return await stat(dir);
   } catch (err) {
     if (err.code === "ENOENT") {
-      await mkdir(SOURCEDIRECTORY);
-      console.log(`Directory created.\n${SOURCEDIRECTORY}`);
+      await mkdir(dir);
+      console.log(`Created newDir.\n${dir}\n`);
     } else {
-      throw new Error(`Failed to access sourceDir.\n${err.message}`);
+      throw new Error(`Failed to access newDir.\n${err.message}\n`);
     }
   }
 }
