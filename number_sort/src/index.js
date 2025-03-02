ec3ff8627a796eb49994c67599fd061ed0939037
permalik <permalik@protonmail.com>
2025-03-02 12:15:17 -0600

    refactor: minimize assert logic

--- number_sort/src/index.js
+++ number_sort/src/index.js
@@ -1,4 +1,4 @@
-import assert, { ok } from "node:assert";
+import assert from "node:assert";
 import { createReadStream } from "node:fs";
 import { readdir, stat, mkdir, rm, open, appendFile } from "node:fs/promises";
 import { createInterface } from "node:readline";
@@ -11,19 +11,20 @@ const DESTDIRECTORY = "number_sort/dest";
 
 async function run() {
   try {
-    const isInitSource = await initDirectory(SOURCEDIRECTORY, inputFiles);
-    assert.equal(isInitSource, true, "Source must be initialized");
-
-    const isInitDest = await initDirectory(DESTDIRECTORY, null);
-    assert.equal(isInitDest, true);
-
-    const isSourcePopulated = await populateSource();
-    assert.equal(isSourcePopulated, true, "Source must be populated");
-
-    const isNumbersSorted = await sortNumbers(SOURCEDIRECTORY, DESTDIRECTORY);
-    assert.equal(isNumbersSorted, true, "Numbers must be sorted");
+    assert.equal(
+      await initDirectory(SOURCEDIRECTORY, inputFiles),
+      true,
+      "Source must be initialized",
+    );
+    assert.equal(await initDirectory(DESTDIRECTORY, null), true);
+    assert.equal(await populateSource(), true, "Source must be populated");
+    assert.equal(
+      await sortNumbers(SOURCEDIRECTORY, DESTDIRECTORY),
+      true,
+      "Numbers must be sorted",
+    );
   } catch (err) {
-    console.error(`Error.\n${err.message}\n`);
+    console.error(`Error from run().\n${err.message}\n`);
   }
 }
 
@@ -51,21 +52,25 @@ async function initDirectory(dirPath, inputFiles) {
     if (inputFiles) {
       if (!dir) {
         for (let [_, v] of Object.entries(inputFiles)) {
-          try {
-            const fileHandle = await open(`${dirPath}/${v}`, "w");
-            await fileHandle.close();
-          } catch (err) {
+          const fileHandle = await open(`${dirPath}/${v}`, "w").catch((err) => {
             throw new Error(
-              `Failed to create and close file ${v}.\n${err.message}\n`,
+              `Failed to open source file ${v}.\n${err.message}\n`,
             );
-          }
+          });
+
+          await fileHandle.close().catch((err) => {
+            throw new Error(
+              `Failed to close dest file ${v}.\n${err.message}\n`,
+            );
+          });
         }
+
         assetsCreated = true;
       }
     } else {
       const dirStat = await stat(dirPath).catch((err) => {
         if (err.code !== "ENOENT") {
-          throw new Error(`Failed to stat DESTDIRECTORY.\n${err.message}\n`);
+          throw new Error(`Failed to stat dest directory.\n${err.message}\n`);
         }
       });
 
@@ -74,47 +79,47 @@ async function initDirectory(dirPath, inputFiles) {
       }
     }
 
-    const promise = new Promise((res, rej) => {
+    return new Promise((res, rej) => {
       if (assetsCreated) {
         res(true);
       } else {
         rej(false);
       }
     });
-
-    return promise;
   } catch (err) {
-    console.error(err);
+    console.error(`Error from ${dirPath} initDirectory().\n${err.Message}\n`);
   }
 }
 
 async function populateSource() {
-  let sourcesPopulated = false;
-  for (let [_, v] of Object.entries(inputFiles)) {
-    let lineCounter = 0;
-    do {
-      await appendFile(
-        `${SOURCEDIRECTORY}/${v}`,
-        `${randomNumberString()}\n`,
-      ).catch((err) => {
-        throw new Error(
-          `Failed to populate line to source directory.\n${err.message}\n`,
-        );
-      });
-      lineCounter++;
-    } while (lineCounter < 100);
-    sourcesPopulated = true;
-  }
-
-  const promise = await new Promise((res, rej) => {
-    if (sourcesPopulated) {
-      res(true);
-    } else {
-      rej(false);
+  try {
+    let sourcesPopulated = false;
+    for (let [_, v] of Object.entries(inputFiles)) {
+      let lineCounter = 0;
+      do {
+        await appendFile(
+          `${SOURCEDIRECTORY}/${v}`,
+          `${randomNumberString()}\n`,
+        ).catch((err) => {
+          throw new Error(
+            `Failed to populate line to source directory.\n${err.message}\n`,
+          );
+        });
+        lineCounter++;
+      } while (lineCounter < 100);
+      sourcesPopulated = true;
     }
-  });
 
-  return promise;
+    return await new Promise((res, rej) => {
+      if (sourcesPopulated) {
+        res(true);
+      } else {
+        rej(false);
+      }
+    });
+  } catch (err) {
+    console.error(`Error from populateSource().\n${err.message}\n`);
+  }
 }
 
 async function sortNumbers(sourceDir, destDir) {
@@ -169,10 +174,9 @@ async function sortNumbers(sourceDir, destDir) {
           );
         },
       );
+
       await fileHandle.close().catch((err) => {
-        throw new Error(
-          `Failed to create and close dest file${v}.\n${err.message}\n`,
-        );
+        throw new Error(`Failed to close dest file${v}.\n${err.message}\n`);
       });
 
       for (const sortedLine of sortedLines) {
@@ -189,15 +193,14 @@ async function sortNumbers(sourceDir, destDir) {
         isSorted = true;
       }
     }
-    const promise = new Promise((res, rej) => {
+
+    return new Promise((res, rej) => {
       if (isSorted) {
         res(true);
       } else {
         rej(false);
       }
     });
-
-    return promise;
   } catch (err) {
     console.error(err);
   }
